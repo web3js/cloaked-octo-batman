1f93edf608bb5037ab6647138e56dd773e6f1cb5
lswlhn <lswlhn@gmail.com>
2014-02-06 16:24:46 -0500

    Update main.js

--- siwen li/assignment-1/main.js
+++ siwen li/assignment-1/main.js
@@ -1,14 +1,37 @@
-var calculate = {
-  add: function (a, b) {
-    return: a+b;
+var myCalculator = {
+  
+  name: Calculator,
+  year_production: 2014,
+  version: CXII,
+  color: black,
+  opertaion : {
+    add: function (a, b) {
+      return: a+b;
+      this.shove(a+b);
+    },
+    subtract: function (a, b) {
+      return: a-b;
+      this.shove(a-b);
+    },
+    multiply: function (a, b) {
+      return: a*b;
+      this.shove(a*b);
+    }
+    divide: function (a, b) {
+      if (b==0) {
+        throw zeroDivision;
+      } else {
+      return: a/b;
+      this.shove(a/b);
+      }
+    };
   },
-  subtract: function (a, b) {
-    return: a-b;
-  },
-  multiply: function (a, b) {
-    return: a*b;
-  }
-  divide: function (a, b) {
-    return: a/b;
+  result: [],
+  shove : function(thing) {
+    this.result.push(thing)
+  }	;
+  zeroDivision: {
+  name: 'zero division',
+  expression: "Sorry, you can't divide a number by 0!"
   }
 }
