9f461ff091b9238c89c8517746be1cb2e2d8407e
Chris Henrick <chrishenrick@gmail.com>
2014-02-06 20:43:12 -0500

    updated index.html and calculator.js

--- chris-henrick/assignment01/js/calculator.js
+++ chris-henrick/assignment01/js/calculator.js
@@ -5,7 +5,7 @@ var calculator = {
 	name: 'Bo-Ris da calculator',
 	version: 0.1,
 
-	add: function(v1,v2){
+	addd: function(v1,v2){
 		this.shove(v1 + v2);
 	},
 	subtract: function(v1,v2){
@@ -31,27 +31,39 @@ var calculator = {
 		message: 'Bo-Ris says your options are: add, subtract, multiply, divide, remainder.'
 	},
 
-	calculate : function(option, v1, v2){
+	input: {
+		listen: function() {
 
-		//to do: do a check for optionException. For Loop?
+			$('.button').on('click', function(e){
+				e.preventDefault();
+				calculator.input.numCheck(calculator.calculate);
+			});
+		},
 
-		var options = ["add","subtract","multiply","divide","remainder"],
-			len = options.length,
-			i = 0;
+		numCheck: function(callback) {
+			var option = $('#option').val(),
+				val1 = $('#value1').val(),
+				val2 = $('#value2').val();
 
-		// for (i; i < len; i++) {
-		// 		if (option !== options[i]){
-		// 			throw this.optionException.message;							
-		// 		}
-		// 	}				
+			if (val1 == "" || val2 == "") {
+				alert("numbers only!");
+			} else {
+				console.log(option + ', ' + val1 + ', ' + val2);
+				callback(option, parseFloat(val1), parseFloat(val2));
+			}
+		}
+	},
+
+	calculate : function(option, v1, v2){				
 
 		if (typeof v1 === "number" && typeof v2 === "number"){
 			switch(option) {
-				case "add" : this.add(v1,v2); break;
+				case "add" : this.addd(v1,v2); break;
 				case "subtract" : this.subtract(v1,v2); break;
 				case "multiply" : this.multiply(v1,v2); break;
 				case "divide" : this.divide(v1,v2); break;
 				case "remainder" : this.remainder(v1,v2); break;
+				default: throw this.optionException.message; break;
 			}
 
 		} else {
@@ -62,7 +74,12 @@ var calculator = {
 	
 	shove : function(result){
 		this.data.push(result);
+		$('#result').text(result);
 	},
 
 	data : []
 }
+
+$(document).ready(function(){
+	calculator.input.listen();
+})
