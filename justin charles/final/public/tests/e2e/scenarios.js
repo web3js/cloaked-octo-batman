0db559beb3fa15a08116c56c3931d4fbf3fc50c9
Justin Charles <j@justincharles.net>
2014-05-16 02:28:10 -0400

    Added final. Thanks for a great semester, Mani.

---
+++ justin charles/final/public/tests/e2e/scenarios.js
@@ -0,0 +1,37 @@
+'use strict';
+
+// http://docs.angularjs.org/guide/dev_guide.e2e-testing
+
+describe('Todo list', function() {
+    beforeEach(function() {
+        browser().navigateTo('/');
+        sleep(1);
+    });
+
+    it("should be able to add a todo", function() {
+        expect(repeater('.not-done').count()).toBe(0);
+        expect(repeater('.done').count()).toBe(0);
+
+        input('newTodo.description').enter('Test todo');
+        element('.btn-primary').click();
+        sleep(1);
+
+        expect(repeater('.not-done').count()).toBe(1);
+        expect(repeater('.done').count()).toBe(0);
+    });
+
+    it("should move todo to correct list when user toggles checkbox", function() {
+        expect(repeater('.not-done').count()).toBe(1);
+        expect(repeater('.done').count()).toBe(0);
+
+        element('.not-done:nth-child(1) input').click();
+        sleep(0.1);
+        expect(repeater('.not-done').count()).toBe(0);
+        expect(repeater('.done').count()).toBe(1);
+
+        element('.done:nth-child(1) input').click();
+        sleep(0.1);
+        expect(repeater('.not-done').count()).toBe(1);
+        expect(repeater('.done').count()).toBe(0);
+    });
+});
